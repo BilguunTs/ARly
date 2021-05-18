2af8a07e13fe025dd3485fb2552d9e23382f1521
BilguunTs <usosketch@yahoo.com>
2021-05-18 17:12:51 +0800

    duder

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text> hello </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
